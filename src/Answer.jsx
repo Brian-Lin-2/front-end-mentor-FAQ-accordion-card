c035a1d77e102165a3a137d98a2f09214bf6b3d2
Brian <linbrian24@gmail.com>
2023-05-10 23:51:52 -0400

    Style FAQ questions section

--- src/Answer.jsx
+++ src/Answer.jsx
@@ -1,7 +1,7 @@
 export default function Answer({ children }) {
   return (
     <>
-      <p>{children}</p>
+      <p className="border-b py-3">{children}</p>
     </>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,46 +3,62 @@ import Answer from './Answer';
 
 export default function Card() {
   return (
-    <div className="border bg-white flex flex-col justify-center items-center">
+    <div className="border bg-white flex flex-col justify-center items-center w-5/6 text-xs">
+      
       <img className="w-72" src="./images/illustration-woman-online-mobile.svg"></img>
-      <h1>FAQ</h1>
+      
+      <h1 className="text-3xl font-bold my-10">FAQ</h1>
 
-      <Question>
-        How many team members can I invite?
-      </Question>
-      <Answer>
-        You can invite up to 2 additional users on the Free plan. There is no limit on 
-        team members for the Premium plan.
-      </Answer>
+      <div className="border px-6">
+        <div className="flex flex-col items-center">
+          <Question>
+            How many team members can I invite?
+          </Question>
+          <Answer>
+            You can invite up to 2 additional users on the Free plan. There is no limit on 
+            team members for the Premium plan.
+          </Answer>
+        </div>
 
-      <Question>
-        What is the maximum file upload size?
-      </Question>
-      <Answer>
-        No more than 2GB. All files in your account must fit your allotted storage space.
-      </Answer>
+        <div className="flex flex-col items-center">
+          <Question>
+            What is the maximum file upload size?
+          </Question>
+          <Answer>
+            No more than 2GB. All files in your account must fit your allotted storage space.
+          </Answer>
+        </div>
 
-      <Question>
-        How do I reset my password?
-      </Question>
-      <Answer>
-        Click “Forgot password” from the login page or “Change password” from your profile page.
-        A reset link will be emailed to you.
-      </Answer>
+        <div className="flex flex-col items-center">
+          <Question>
+            How do I reset my password?
+          </Question>
+          <Answer>
+            Click “Forgot password” from the login page or “Change password” from your profile page.
+            A reset link will be emailed to you.
+          </Answer>
+        </div>
 
-      <Question>
-        Can I cancel my subscription?
-      </Question>
-      <Answer>
-        Yes! Send us a message and we’ll process your request no questions asked.
-      </Answer>
+        <div className="flex flex-col items-center">
+          <Question>
+            Can I cancel my subscription?
+          </Question>
+          <Answer>
+            Yes! Send us a message and we’ll process your request no questions asked.
+          </Answer>        
+        </div>
 
-      <Question>
-        Do you provide additional support?
-      </Question>
-      <Answer>
-        Chat and email support is available 24/7. Phone lines are open during normal business hours.
-      </Answer>
+
+        <div className="flex flex-col items-center">
+          <Question>
+            Do you provide additional support?
+          </Question>
+          <Answer>
+            Chat and email support is available 24/7. Phone lines are open during normal business hours.
+          </Answer>  
+        </div>
+      </div>
+
     </div>
   )
 }
