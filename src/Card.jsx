2405e89d4e06238148cc335f2b7e2449ebc29a63
Brian <linbrian24@gmail.com>
2023-05-11 23:19:11 -0400

    Fix sizing issues

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,14 +3,14 @@ import Answer from './Answer';
 
 export default function Card() {
   return (
-    <div className="bg-white pb-12 pt-24 sm:p-0 my-20 rounded-3xl flex flex-col sm:flex-row justify-center items-center w-5/6 text-xs bg-mobile-pattern sm:bg-none bg-no-repeat bg-top">
+    <div className="bg-white pb-12 pt-24 sm:p-0 my-20 sm:m-0 rounded-3xl flex flex-col sm:flex-row justify-center items-center w-5/6 sm:w-4/6 text-xs bg-mobile-pattern sm:bg-none bg-no-repeat bg-top">
 
       {/* For desktop state */}
-      <div className="border sm:min-h-screen sm:w-5/12 sm:bg-desktop-pattern sm:bg-no-repeat sm:bg-right sm:bg-auto"></div>
+      <div className="border sm:w-5/12 sm:bg-desktop-pattern sm:bg-no-repeat sm:bg-right sm:bg-auto"></div>
 
-      <img className="w-60 absolute sm:start-0 sm:w-5/12" src="./images/illustration-woman-online-mobile.svg"></img>
+      <img className="w-60 absolute top-12 sm:top-1/4 sm:start-32 sm:w-4/12" src="./images/illustration-woman-online-mobile.svg"></img>
 
-      <div className="px-8 sm:px-40 sm:w-7/12 border">
+      <div className="px-8 sm:pl-14 sm:pr-28 sm:w-7/12 border">
         <h1 className="text-3xl font-bold mt-8 mb-4 text-center sm:text-start">FAQ</h1>
 
         <div className="flex flex-col items-center">
