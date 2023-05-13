3e7d6ad0efae29a1848ea5355e8904b106f33715
Brian <linbrian24@gmail.com>
2023-05-13 10:22:42 -0400

    Finish base desktop design

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,11 +6,11 @@ export default function Card() {
     <div className="bg-white pb-12 pt-24 sm:p-0 my-20 sm:m-0 rounded-3xl flex flex-col sm:flex-row justify-center items-center w-5/6 sm:w-4/6 text-xs bg-mobile-pattern sm:bg-none bg-no-repeat bg-top">
 
       {/* For desktop state */}
-      <div className="border sm:w-5/12 sm:bg-desktop-pattern sm:bg-no-repeat sm:bg-right sm:bg-auto"></div>
+      <div className="sm:w-1/2 sm:bg-desktop-pattern sm:bg-no-repeat sm:bg-right sm:bg-auto"></div>
 
       <img className="w-60 absolute top-12 sm:top-1/4 sm:start-32 sm:w-4/12" src="./images/illustration-woman-online-mobile.svg"></img>
 
-      <div className="px-8 sm:pl-14 sm:pr-28 sm:w-7/12 border">
+      <div className="px-8 sm:pl-14 sm:pr-28 sm:w-1/2">
         <h1 className="text-3xl font-bold mt-8 mb-4 text-center sm:text-start">FAQ</h1>
 
         <div className="flex flex-col items-center">
--- src/Question.jsx
+++ src/Question.jsx
@@ -2,7 +2,7 @@ export default function Question({ children }) {
   return (
       <>
         <div id="faq-clicker" className="flex justify-between items-center border-b w-full py-4">
-          <h3>{children}</h3>
+          <h3 className="text-sm opacity-75">{children}</h3>
           <img src="/images/icon-arrow-down.svg" />
         </div>
       </>
